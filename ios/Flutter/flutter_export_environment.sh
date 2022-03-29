9a213a942b606fbde95107654d197269313df30b
Kutay Akgün <56596454+kutayakgn@users.noreply.github.com>
2022-03-29 19:06:05 +0300

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\kutay\development\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\kutay\first_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\kutay\development\projects\e_reg_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
