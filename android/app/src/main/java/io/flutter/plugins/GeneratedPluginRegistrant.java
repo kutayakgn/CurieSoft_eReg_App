d013b6772a7f18bb35921909bb884f2300440650
Kutay Akgün <56596454+kutayakgn@users.noreply.github.com>
2022-05-27 12:32:00 +0300

    Add files via upload

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -31,9 +31,24 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin firebase_core, io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.storage.FlutterFirebaseStoragePlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin firebase_storage, io.flutter.plugins.firebase.storage.FlutterFirebaseStoragePlugin", e);
+    }
+    try {
+      flutterEngine.getPlugins().add(new com.example.imagegallerysaver.ImageGallerySaverPlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin image_gallery_saver, com.example.imagegallerysaver.ImageGallerySaverPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
     } catch(Exception e) {
       Log.e(TAG, "Error registering plugin path_provider_android, io.flutter.plugins.pathprovider.PathProviderPlugin", e);
+    }
+    try {
+      flutterEngine.getPlugins().add(new com.baseflow.permissionhandler.PermissionHandlerPlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin permission_handler_android, com.baseflow.permissionhandler.PermissionHandlerPlugin", e);
     }
   }
 }
--- lib/screens/signaturePage/signature.dart
+++ lib/screens/signaturePage/signature.dart
@@ -1,20 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:flutter_login_ui/screens/event_detail.dart';
 import 'package:signature/signature.dart';
 
 import '../../utilities/constants.dart';
 
-
 class SignaturePage extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
-
 }
 
-
 class _MyAppState extends State<SignaturePage> {
-
   final SignatureController _controller = SignatureController(
     penStrokeWidth: 1,
     penColor: Colors.red,
@@ -27,43 +29,245 @@ class _MyAppState extends State<SignaturePage> {
   void initState() {
     super.initState();
     _controller.addListener(() => print('Value changed'));
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      setState(() {});
+    });
+  }
+
+  showAlertDialog(BuildContext context) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EventDetailScreen();
+          }),
+        );
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      // title: Text("My title"),
+      content: Text("Your signature successfully saved."),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) => Scaffold(
-          appBar: AppBar(
-            title: Text("Signature page"),
-            actions: [
-              Icon(Icons.more_vert)
-            ],
-            backgroundColor: koyumavi,
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
           ),
-          body: ListView(
-            children: <Widget>[
-              Container(
-                height: 150,
-                child: const Center(
-                  child: Text('Draw your signature to the below, please!'),
+        ),
+        title: Text(
+          "Signature Page",
+        ),
+        centerTitle: true,
+        actions: [
+          // Icon(Icons.more_vert)
+        ],
+        backgroundColor: koyumavi,
+      ),
+      body: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              alignment: Alignment.topCenter,
+              height: 100,
+              child: const Center(
+                child: Text(
+                  'Please sign below!',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black),
                 ),
               ),
-              //SIGNATURE CANVAS
-              Signature(
-                controller: _controller,
-                height: 300,
-                backgroundColor: Colors.white,
-              ),
-              //OK AND CLEAR BUTTONS
-              Container(
-                decoration: const BoxDecoration(color: koyumavi),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    //SHOW EXPORTED IMAGE IN NEW ROUTE
-                    IconButton(
+            ),
+          ),
+
+          //SIGNATURE CANVAS
+          Signature(
+            controller: _controller,
+            height: 300,
+            backgroundColor: Colors.white,
+          ),
+          Spacer(),
+          //OK AND CLEAR BUTTONS
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: const BoxDecoration(color: koyumavi),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  //SHOW EXPORTED IMAGE IN NEW ROUTE
+
+                  SizedBox.fromSize(
+                    size: Size(70, 60),
+                    child: Material(
+                      color: koyumavi,
+                      child: InkWell(
+                        splashColor: Colors.white24,
+                        onTap: () {
+                          setState(() => _controller.clear());
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.clear,
+                              color: Colors.white,
+                            ), // <-- Icon
+                            Text("Clear",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )), // <-- Text
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox.fromSize(
+                    size: Size(70, 60),
+                    child: Material(
+                      color: koyumavi,
+                      child: InkWell(
+                        splashColor: Colors.white24,
+                        onTap: () {
+                          setState(() => _controller.undo());
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.undo,
+                              color: Colors.white,
+                            ), // <-- Icon
+                            Text("Undo",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )), // <-- Text
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox.fromSize(
+                    size: Size(70, 60),
+                    child: Material(
+                      color: koyumavi,
+                      child: InkWell(
+                        splashColor: Colors.white24,
+                        onTap: () {
+                          setState(() => _controller.redo());
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.redo,
+                              color: Colors.white,
+                            ), // <-- Icon
+                            Text("Redo",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )), // <-- Text
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox.fromSize(
+                    size: Size(70, 60),
+                    child: Material(
+                      color: koyumavi,
+                      child: InkWell(
+                        splashColor: Colors.white24,
+                        onTap: () async {
+                          if (_controller.isNotEmpty) {
+                            final Uint8List? data =
+                                await _controller.toPngBytes();
+                            File.fromRawPath(data!);
+                            final FirebaseStorage _firebaseStorage =
+                                FirebaseStorage.instance;
+                            Reference ref = _firebaseStorage
+                                .ref()
+                                .child('${DateTime.now()}.png');
+                            UploadTask uploadTask = ref.putData(data,
+                                SettableMetadata(contentType: 'image/png'));
+                            TaskSnapshot taskSnapshot = await uploadTask
+                                .whenComplete(() => print('done'))
+                                .catchError(
+                                    (error) => print("something went wrong"));
+                            String url =
+                                await taskSnapshot.ref.getDownloadURL();
+
+                            showAlertDialog(context);
+                            /*     if (data != null) {
+                                await Navigator.of(context).push(
+                                  MaterialPageRoute<void>(
+                                    builder: (BuildContext context) {
+                                      return Scaffold(
+                                        appBar: AppBar(),
+                                        body: Center(
+                                          child: Container(
+                                            color: Colors.grey[300],
+                                            child: Image.memory(data),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                );
+                              } */
+                          }
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.check,
+                              color: Colors.white,
+                            ), // <-- Icon
+                            Text("Save",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                )), // <-- Text
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  /*   IconButton(
                       icon: const Icon(Icons.check),
                       color: Colors.white,
                       onPressed: () async {
@@ -111,19 +315,18 @@ class _MyAppState extends State<SignaturePage> {
                       onPressed: () {
                         setState(() => _controller.clear());
                       },
-                    ),
-                  ],
-                ),
+                    ), */
+                ],
               ),
-              /* Container(
+            ),
+            /* Container(
                 height: 300,
                 child: const Center(
                   child: Text('Big container to test scrolling issues'),
                 ),
               ),*/
-            ],
           ),
-        ),
+        ],
       ),
     );
   }
